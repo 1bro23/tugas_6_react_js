18f587fbd08259c5cab4ca62dc6cade343a0f06f
1bro23 <shinlia21@gmail.com>
2020-12-18 00:33:35 +0800

    add tugas 6 react js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+import MenuHome from "./Page/MenuHome";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <MenuHome/>
+        <p>sadasdas</p>
+        <br/>
+      </div>
+    )
+  }
+}
+export default App;
---
+++ src/Lib/DaftarMakanan.js
@@ -0,0 +1,28 @@
+const DaftarMakanan=[
+  {
+    url:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg",
+    NamaMakanan:"Nasi Padang",
+    Harga:"20.000"
+  },
+  {
+    url:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png",
+    NamaMakanan:"Sate",
+    Harga:"20.000"
+  },
+  {
+    url:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png",
+    NamaMakanan:"Soto",
+    Harga:"10.000"
+  },
+  {
+    url:"https://www.dbs.com/iwov-resources/images/newsroom/indonesia/Blog/masakan%20nusantara/nasi%20kentut.png",
+    NamaMakanan:"Uduk",
+    Harga:"10.000"
+  },
+  {
+    url:"https://blog.tokowahab.com/wp-content/uploads/2020/03/Resep-Nasi-Kuning-Tumpeng-Mini.jpg ",
+    NamaMakanan:"Nasi Kuning",
+    Harga:"15.000"
+  }
+]
+export default DaftarMakanan;
